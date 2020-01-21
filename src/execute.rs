4624c73ae7db1f93e1d9124e69285ce6905f1089
Jonas Natten <jonas@natten.it>
2020-01-21 10:38:41 +0100

    Clean up printing of output

--- src/execute.rs
+++ src/execute.rs
@@ -6,6 +6,12 @@ use super::mrt_errors::MrtError;
 use super::mrt_errors;
 use colored::Colorize;
 
+struct ExecutionOutput {
+    exit_code: i32,
+    stdout: String,
+    stderr: String,
+}
+
 fn get_all_paths(tags: Vec<String>, config: ConfigFile) -> Vec<String> {
     let mut all_paths: Vec<String> = tags.iter().flat_map(|t| {
         let tag_without_prefix: &str = t.as_str()[1..].as_ref(); // TODO: slice this in a better way, this may panic!!!
@@ -24,6 +30,19 @@ fn get_all_paths(tags: Vec<String>, config: ConfigFile) -> Vec<String> {
     all_paths
 }
 
+fn print_result(path: String, output: ExecutionOutput) -> () {
+    let headline = format!("in {}", path.as_str());
+    if output.exit_code == 0 {
+        println!("{}", headline.bright_black());
+    } else {
+        let code = format!("{}", output.exit_code);
+        println!("{} ({})", headline.bright_black(), code.red());
+    }
+
+    if output.stdout.len() > 0 { println!("\n{}", output.stdout); }
+    if output.stderr.len() > 0 { eprintln!("\n{}", output.stderr.red()); }
+}
+
 pub fn exec(parsed_args: ParsedArgs, config: ConfigFile) -> Result<i8, MrtError> {
     // TODO: Parallelization
 
@@ -39,27 +58,36 @@ pub fn exec(parsed_args: ParsedArgs, config: ConfigFile) -> Result<i8, MrtError>
             let all_paths = get_all_paths(parsed_args.tags, config);
 
             for path in all_paths {
-                let result = exec_at_path(&mut cmd, &path);
+                match exec_at_path(&mut cmd, &path) {
+                    Ok(res) => print_result(path, res),
+                    _ => ()
+                }
             }
             Ok(0) // TODO: Somehow handle errors, maybe map over rather than for loop and print all
         }
     }
 }
 
-fn exec_at_path(cmd: &mut Command, path: &String) -> Result<(i32, String), MrtError> {
+fn exec_at_path(cmd: &mut Command, path: &String) -> Result<ExecutionOutput, MrtError> {
     cmd.current_dir(&path);
     match cmd.output() {
         Ok(output) => {
-            let output_string = std::str::from_utf8(&output.stdout);
-            match output_string {
-                Ok(out) => {
+            let stdout_string = std::str::from_utf8(&output.stdout);
+            let stderr_string = std::str::from_utf8(&output.stderr);
+            match (stdout_string, stderr_string) {
+                (Ok(out), Ok(err)) => {
                     let exit_code: i32 = match output.status.code() {
                         Some(int) => int,
                         _ => -255
                     };
-                    let headline = format!("in {}", path.as_str());
-                    println!("{}\n\n{}", headline.bright_black(), out);
-                    Ok((exit_code, String::from(out)))
+
+                    let execution = ExecutionOutput {
+                        exit_code,
+                        stdout: String::from(out),
+                        stderr: String::from(err),
+                    };
+
+                    Ok(execution)
                 }
                 _ => {
                     println!("Couldn't convert output to string...");
