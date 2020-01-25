d5f93d887c9535afce50ec2dad699e701d6bcd89
Jonas Natten <jonas@natten.it>
2020-01-25 22:27:46 +0100

    Adds test for saving and loading config

--- src/config/configmodels.rs
+++ src/config/configmodels.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub struct ConfigFile {
     pub version: String,
     pub tags: HashMap<String, Tag>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub struct Tag {
     pub paths: Vec<String>,
 }
--- src/config/loader.rs
+++ src/config/loader.rs
@@ -17,16 +17,16 @@ pub fn load_config(path: &String) -> Result<ConfigFile> {
 
 pub fn save_config(config: ConfigFile) -> Result<(ConfigFile)> {
     let config_path = get_config_path().unwrap_or(String::from(".mrtconfig.json"));
-    save_config_at(&config_path, config)
+    save_config_at(&config_path, &config).map(|()| config)
 }
 
-fn save_config_at(path: &String, config_struct: ConfigFile) -> Result<(ConfigFile)> {
-    let data = serde_json::to_string_pretty(&config_struct)?;
+fn save_config_at(path: &String, config_struct: &ConfigFile) -> Result<()> {
+    let data = serde_json::to_string_pretty(config_struct)?;
 
     let mut file = File::create(path.as_str())?;
     file.write_all(data.as_bytes())?;
 
-    Ok(config_struct)
+    Ok(())
 }
 
 pub fn create_new_empty_config(path: &String) -> Result<(ConfigFile)> {
@@ -35,7 +35,7 @@ pub fn create_new_empty_config(path: &String) -> Result<(ConfigFile)> {
         tags: HashMap::new(),
     };
 
-    save_config_at(path, new_config)
+    save_config_at(path, &new_config).map(|()| new_config)
 }
 
 fn read_file_to_string(path: &String) -> Result<String> {
@@ -64,4 +64,38 @@ pub fn get_config_path() -> Option<String> {
     };
 
     config_path
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_read_write_config_file() -> Result<()> {
+        let dir = tempdir::TempDir::new("mrttest")?;
+        let dir_path = dir.path();
+        let test_config_path = dir_path.join("config.json").to_string_lossy().to_string();
+
+        let tag_path1 = dir_path.join("test1").to_string_lossy().to_string();
+        let tag_path2 = dir_path.join("test2").to_string_lossy().to_string();
+
+        let tag_to_save = Tag {
+            paths: vec![tag_path1, tag_path2],
+        };
+
+        let mut tags = HashMap::new();
+        tags.insert(String::from("testtag"), tag_to_save);
+
+        let config_to_save = ConfigFile {
+            version: crate::APP_VERSION.to_owned(),
+            tags,
+        };
+
+        save_config_at(&test_config_path, &config_to_save)?;
+        let read_config = load_config(&test_config_path)?;
+
+        assert_eq!(config_to_save, read_config);
+        dir.close()?;
+        Ok(())
+    }
 }
