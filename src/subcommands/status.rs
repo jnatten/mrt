df47455c5f52cc59f1be3d086bb9af10a22905f3
Jonas Natten <jonas@natten.it>
2020-02-11 09:30:35 +0100

    Adds behindness to status command

--- src/subcommands/status.rs
+++ src/subcommands/status.rs
@@ -2,6 +2,7 @@ use super::super::argparse::ParsedArgs;
 use super::super::config::configmodels::ConfigFile;
 use super::super::execute;
 use clap::ArgMatches;
+use colored::Colorize;
 use std::process::Command;
 
 pub fn status(args: &ArgMatches, parsed_arguments: &ParsedArgs, config: ConfigFile) {
@@ -31,10 +32,14 @@ fn format_output(path: &String, out: &Vec<u8>) -> String {
     let x = String::from_utf8_lossy(out).to_string();
     let y: Vec<String> = x.split('\n').map(String::from).collect();
     let branch: String = get_branch(&y).unwrap_or(String::from("<UNKNOWN>"));
+    let behindness: String = get_behindness(&y)
+        .map(|b| format!(" {}", b.yellow()))
+        .unwrap_or(String::new());
 
-    let spaces_to_add = 50 - path.len();
-    let spaces = " ".repeat(spaces_to_add);
-    format!("{}{}{}", path, spaces, branch) // TODO: Expand with dirtyness
+    let path_spaces_to_add = 50 - path.len();
+    let path_spaces = " ".repeat(path_spaces_to_add);
+
+    format!("{}{}{}{}", path, path_spaces, branch, behindness) // TODO: Expand with dirtyness
 }
 
 fn get_branch(lines: &Vec<String>) -> Option<String> {
@@ -56,6 +61,20 @@ fn get_branch(lines: &Vec<String>) -> Option<String> {
     })
 }
 
+fn get_behindness(lines: &Vec<String>) -> Option<String> {
+    lines
+        .first()
+        .map(|branch_line| {
+            if branch_line.ends_with("]") {
+                let mut split: Vec<String> = branch_line.split(" [").map(String::from).collect();
+                split.pop().map(|l| format!("[{}", l))
+            } else {
+                None
+            }
+        })
+        .flatten()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -63,19 +82,40 @@ mod test {
     fn to_string_vec(v: Vec<&str>) -> Vec<String> {
         v.into_iter().map(|s| s.to_owned()).collect()
     }
+    #[test]
+    fn test_get_behindness_func() {
+        let input1 = to_string_vec(vec!["## mas...[ter...origin/mas...[ter [behind 1]"]);
+        let input2 = to_string_vec(vec!["## master...origin/master [behind 2]"]);
+        let input3 = to_string_vec(vec!["## mas...[ter...origin/mas...[ter"]);
+        let input4 = to_string_vec(vec!["## master...origin/master"]);
+
+        let expected1 = String::from("[behind 1]");
+        let expected2 = String::from("[behind 2]");
+
+        assert_eq!(get_behindness(&input1), Some(expected1));
+        assert_eq!(get_behindness(&input2), Some(expected2));
+        assert_eq!(get_behindness(&input3), None);
+        assert_eq!(get_behindness(&input4), None);
+    }
 
     #[test]
     fn test_get_branch_func() {
         let input1 = to_string_vec(vec!["## master...origin/master"]);
         let input2 = to_string_vec(vec!["## mas## ter...origin/mas## ter"]);
         let input3 = to_string_vec(vec!["## mas...## ter...origin/mas...## ter"]);
+        let input4 = to_string_vec(vec!["## mas...[ter...origin/mas...[ter [behind 1]"]);
+        let input5 = to_string_vec(vec!["## master...origin/master [behind 1]"]);
 
         let expected1 = String::from("master");
         let expected2 = String::from("mas## ter");
         let expected3 = String::from("mas...## ter");
+        let expected4 = String::from("mas...[ter");
+        let expected5 = String::from("master");
 
         assert_eq!(get_branch(&input1), Some(expected1));
         assert_eq!(get_branch(&input2), Some(expected2));
         assert_eq!(get_branch(&input3), Some(expected3));
+        assert_eq!(get_branch(&input4), Some(expected4));
+        assert_eq!(get_branch(&input5), Some(expected5));
     }
 }
